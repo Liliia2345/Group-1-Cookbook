1c071bf72184dd1bb89866800fa6dd2f5277f44f
David Niermann <dc.niermann@gmail.com>
2023-02-07 21:41:49 +0100

    dotenv

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -3,9 +3,13 @@ const contentful = require("contentful");
 
 export default function App() {
   const [recipes, setRecipes] = useState([]);
-  const SPACE_ID = "gll392hbsiba";
+  /*   const SPACE_ID = "gll392hbsiba";
   const ENVIRONMENT_ID = "master";
-  const ACCESS_TOKEN = "uRx-_ftxy7bxQ0HBNR21Wd4zMnwguYt5HjlGaV_3lbI";
+  const ACCESS_TOKEN = "uRx-_ftxy7bxQ0HBNR21Wd4zMnwguYt5HjlGaV_3lbI"; */
+
+  const SPACE_ID = process.env.REACT_APP_SPACE_ID;
+  const ENVIRONMENT_ID = process.env.REACT_APP_ENVIRONMENT_ID;
+  const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
 
   useEffect(() => {
     const client = contentful.createClient({
