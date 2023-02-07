7af1a3e17e51b6646f2669b16be8a715678664a7
Liliia Shvorob <114217884+Liliia2345@users.noreply.github.com>
2023-02-08 00:22:31 +0100

    Revert "Apistuff"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import "./App.css";
-import Header from "./components/Header";
-import Search from "./components/Search";
-import Footer from "./components/Footer";
-import Recipes from "./components/Recipes";
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Search from './components/Search';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Search />
-      <Recipes />
       <Footer />
     </div>
   );
