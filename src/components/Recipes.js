e7132b32a8469a470b89144761830fee538d8923
David Niermann <dc.niermann@gmail.com>
2023-02-09 15:03:49 +0100

    remove process.env. prefix

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,9 +6,9 @@ export default function App() {
 
   useEffect(() => {
     const client = contentful.createClient({
-      space: process.env.REACT_APP_SPACE_ID,
-      accessToken: process.env.REACT_APP_ACCESS_TOKEN,
-      environment: process.env.REACT_APP_ENVIRONMENT_ID,
+      space: REACT_APP_SPACE_ID,
+      accessToken: REACT_APP_ACCESS_TOKEN,
+      environment: REACT_APP_ENVIRONMENT_ID,
     });
     client
       .getEntries()
