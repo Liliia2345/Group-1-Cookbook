654fc591238af5b3d3dd9cc522311e293d11fc13
unknown <aysamuel007@gmail.com>
2023-02-09 03:01:12 +0100

    updatesearchandaddedfooter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer class='bg-warning text-center text-white mt-5'>
+      <div class='container p-3 pb-0'>
+        <section class=''>
+          <form action=''>
+            <div class='row d-flex justify-content-center'>
+              <div class='col-auto'>
+                <p class='pt-2 text-black'>
+                  <strong>Sign up for our newsletter</strong>
+                </p>
+              </div>
+              <div class='col-md-5 col-12'>
+                <div class='form-outline form-white mb-4'>
+                  <input
+                    type='email'
+                    id='form5Example29'
+                    class='form-control'
+                  />
+                  <label
+                    class='form-label text-black'
+                    for='form5Example29'>
+                    Email address
+                  </label>
+                </div>
+              </div>
+              <div class='col-auto'>
+                <button
+                  type='submit'
+                  class='btn btn-outline-dark mb-4 text-black'>
+                  Subscribe
+                </button>
+              </div>
+            </div>
+          </form>
+        </section>
+      </div>
+      <div
+        class='text-center text-black p-3'
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2023 Copyright:{' '}
+        <a
+          class='text-black'
+          href='https://mdbootstrap.com/'>
+          Perfect Burgers
+        </a>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,20 +6,22 @@ function Search() {
   const [searchTerm, setSearchTerm] = useState('');
 
   return (
-    <div className='search'>
-      <input
-        placeholder='Search for recipes'
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            // Call the API search function with the current searchTerm value
-          }
-        }}></input>
-      <img
-        src={SearchIcon}
-        alt='search'
-        onClick={() => searchTerm}></img>
+    <div className='ccontainer mb-3'>
+      <div className='search'>
+        <input
+          placeholder='Search for recipes'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              // Call the API search function with the current searchTerm value
+            }
+          }}></input>
+        <img
+          src={SearchIcon}
+          alt='search'
+          onClick={() => searchTerm}></img>
+      </div>
     </div>
   );
 }
