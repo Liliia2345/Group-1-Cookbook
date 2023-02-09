4de686d0db512797e51858e9af2999e6185de9aa
unknown <aysamuel007@gmail.com>
2023-02-09 18:51:39 +0100

    lss

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,6 +5,7 @@ const contentful = require('contentful');
 
 function Search() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [recipes, setRecipes] = useState([]);
   const client = contentful.createClient({
     space: process.env.REACT_APP_SPACE_ID,
     accessToken: process.env.REACT_APP_ACCESS_TOKEN,
